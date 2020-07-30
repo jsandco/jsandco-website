9904333c5e064fae68c62ff4d38b62612039e08f
absolom <olivier@make-it.fr>
2020-07-30 18:49:09 +0200

    add null publication for current user

--- imports/api/users/server/publications.js
+++ imports/api/users/server/publications.js
@@ -1,4 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish(null, () => {
+Meteor.publish(null, function () {
+  if (this.userId) {
+    return Meteor.users.find(this.userId, {
+      fields: {
+        username: 1,
+        emails: 1,
+        firstname: 1,
+        lastname: 1,
+        photo: 1,
+        role: 1,
+      },
+      sort: { _id: 1 },
+      limit: 1,
+    });
+  }
+  this.ready();
+  return null;
 });
